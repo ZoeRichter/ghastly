40fcf8198420ee641f95a7045d0d49460cf4b4ce
Zoe R <zoerichter96@gmail.com>
2024-06-25 13:44:40 -0500

    test pf_to_n/n_to_pf, added notes for other tests

--- tests/test_pack.py
+++ tests/test_pack.py
@@ -1,7 +1,7 @@
-import pytest
-import didymus as di
+import pytest as pyt
+import didymus as ddm
 
-test_core = di.core.CylCore(10.0,50.0,np.array([3.0, 6.0, 9.0]))
+#test_core = di.core.CylCore(10.0,50.0,np.array([3.0, 6.0, 9.0]))
 
 def test_pebble_packing():
 	'''
@@ -12,21 +12,50 @@ def test_pebble_packing():
 	
 def test_pf_to_n():
 	'''
+    Tests pack.pf_to_n()
 	'''
-	pass
-	return
+    core_h = 1
+    core_r = 0.5
+    pebble_r = 0.5
+    pf = 2/3
+    core_vol = np.pi*(core_r**2)*core_h
+    pebble_vol = (4/3)*np.pi*(pebble_r**3)
+    N = (pf*core_vol)/pebble_vol
+    test_core = ddm.core.CylCore(core_r,core_h)
+    assert ddm.pack.pf_to_n(test_core, pebble_r, pf) == pyt.approx(N)
 
 def test_n_to_pf():
 	'''
+    Tests pack.n_to_pf()
 	'''
-	pass
-	return
+	core_h = 1
+    core_r = 0.5
+    pebble_r = 0.5
+    N = 1
+    core_vol = np.pi*(core_r**2)*core_h
+    pebble_vol = (4/3)*np.pi*(pebble_r**3)
+    pf = (pebble_vol*N)/core_vol
+    test_core = ddm.core.CylCore(core_r,core_h)
+    assert ddm.pack.n_to_pf(test_core, pebble_r, N) == pyt.approx(pf)
 	
 def test_find_start_coords():
 	'''
 	'''
-	pass
-	return
+	#make sure length of starting coords is N
+    #make sure points in core
+    #currently function assumes you won't get repeats - enough of the factors
+    #are randomly generated that I think that's fair
+    #could, using the 7357 seed, find the first starting coord as function
+    #does, and make sure it matches the first item in start_coords?
+    #but if you don't reset the generator, I think it wouldn't be, so
+    #find_start_coords
+    #test_rng = 7357
+    #find first coord same way find start coords does
+    #make sure they are equal
+    #^but is this a useful test
+    #could you check for unifrom randomness by calculating the centroid of the
+    #start coords point cloud, and making sure it is in the area of the core
+    #origin?
 
 def test_jt_algorithm():
 	'''
@@ -43,17 +72,24 @@ def test_nearest_neighbor():
 def test_select_pair():
 	'''
 	'''
-	pass
-	return
+	# check order of points (1, 2), not (2,1), 
+    #check you get two points in a tuple
 
 def test_mesh_grid():
 	'''
 	'''
-	pass
-	return
+	#if you make a specific core, and feed it a delta/short list of coords
+    #you should be able to hand-calculate the grid IDs with relative ease
+    #and confirm the function is getting the same
 	
 def test_move():
 	'''
 	'''
-	pass
-	return
+	#if you give a specific core, two points that you define explicitly, 
+    #and a simple d_out, you should be able to confirm that the function moves
+    #them apart and keeps them in the core
+    #do one test to just move, do one test that you know will move them out of
+    #bounds, and check that it 1) moves them back in bounds
+    # 2) repeats move until it is d_out apart (if you have the pebbles line up
+    #on an axis, you should be able to hand determine exactly what move will
+    #do, down to the number of iterations
