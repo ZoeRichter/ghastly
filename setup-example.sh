31166ca09cca80d52bd1f6f9f83fb2e82b533443
Zoe <39283555+ZoeRichter@users.noreply.github.com>
2026-01-30 15:19:07 -0600

    Apply suggestions from code review
    Co-authored-by: Liam Pohlmann <118499715+LP1012@users.noreply.github.com>

--- setup-example.sh
+++ setup-example.sh
@@ -5,10 +5,10 @@
 
 #before running this script, you should have all the zip files/tarballs/etc
 #this references downloaded and extracted, and you will need build essentials, conda, git,
-# cmake, and a conda env called ghastly-dev created
+#cmake, and a conda env called ghastly-dev created
 
 #you need to download:
-#hdf5 zip
+#hdf5 source code
 
 source activate base
 conda activate ghastly-dev
@@ -29,19 +29,25 @@ conda install ffmpeg
 conda install libjpeg-turbo
 conda install uncertainties
 conda install lxml
+
 cd hdf5_version/ # update this line with the correct hdf5 directory name
+
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DHDF5_ENABLE_PARALLEL=ON -DHDF5_BUILD_CPP_LIB=OFF -DHDF5_ENABLE_Z_LIB_SUPPORT=ON -DZLIB_LIBRARY:FILEPATH=${CONDA_PREFIX}/lib/libz.so -DZLIB_INCLUDE_DIR:PATH=${CONDA_PREFIX}/include -DZLIB_USE_EXTERNAL=OFF -DHDF5_ENABLE_SZIP_SUPPORT=OFF ..
 cd ..
 cmake --build ./build --config Release -j 4
 cmake --install ./build --config Release
+
 conda deactivate
 conda activate ghastly-dev
+
 h5dump --version
+
 python -m pip --version
 python -m pip install mpi4py
 cc=${CONDA_PREFIX} HDF5_MPI=ON HDF5_DIR=${CONDA_PREFIX} python -m pip install --no-binary=h5py h5py
 python -c "import h5py; print(h5py.version.hdf5_version)"
+
 git clone --recurse-submodules https://github.com/openmc-dev/openmc.git
 cd ~/openmc/
 mkdir build && cd build
@@ -49,8 +55,12 @@ HDF5_ROOT=${CONDA_PREFIX} cmake -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} -DOPENMC_
 make
 make install
 cd ..
+
+
 python -m pip install -e .
 cd ~
+
+
 git clone -b release https://github.com/lammps/lammps.git lammps_install
 cd lammps_install/
 mkdir build && cd build
@@ -58,7 +68,9 @@ cmake -C ../cmake/presets/most.cmake -D CMAKE_INSTALL_PREFIX=${CONDA_PREFIX} -D 
 make
 make install
 cd ~
+
 git clone git@github.com:arfc/ghastly.git
 cd ghastly/
 python -m pip install --editable .
 echo "Done!"
+
