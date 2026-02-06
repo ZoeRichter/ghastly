9fb4e29d8ec34cfecc7cfc11072e5a29aac9df0c
ZoeRichter <zoerichter96@gmail.com>
2026-02-06 16:19:08 -0600

    updated docs and tests, added tests for main functions

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,17 @@
 import pytest
 import numpy as np
+import glob
+import os
 from ghastly import main
 from ghastly import read_input
 
+test_input = read_input.InputBlock("fill_input.json")
+test_sim = test_input.create_obj()
 
 def test_pack_core():
     '''
     Tests the main function pack_cyl.
     '''
-    test_input = read_input.InputBlock("sample_input.json")
-    test_sim = test_input.create_obj()
     test_cyl = test_sim.core_main["main_cyl"]
     test_coords = main._pack_core(test_sim, 
                                   test_cyl,
@@ -32,11 +34,52 @@ def test_find_box_bounds():
     '''
     Tests the find_box_bounds function in main.py.
     '''
-    test_input = read_input.InputBlock("sample_input.json")
-    test_sim = test_input.create_obj()
     bound_limits = main.find_box_bounds(test_sim)
 
     assert bound_limits['xb_min'] == pytest.approx(0.05 -0.5 - 5*0.03)
     assert bound_limits['yb_max'] == pytest.approx(0.5 + 5*0.03)
     assert bound_limits['zb_min'] == pytest.approx(-0.1 - 5*0.03)
     assert bound_limits['zb_max'] == pytest.approx(1.2 + 5*0.03)
+
+
+def test_write_variable_block():
+    '''
+    Tests _write_variable_block in main.py.
+    '''
+    variables = main._write_variable_block(test_input, test_sim,
+                                           "variables.txt", 
+                                           "lammps/variable_template.txt")
+
+    assert variables['r_vessel'] == pytest.approx(0.5)
+    assert variables['vessel_zmin'] == pytest.approx(-0.1)
+    assert variables['vessel_zmax'] == pytest.approx(1.0)
+    assert variables['vessel_x_c'] == pytest.approx(0.05)
+    assert variables['vessel_y_c'] == pytest.approx(0.0)
+
+def test_fill_core():
+    '''
+    Tests fill_core in main.py.
+    '''
+    main.fill_core("fill_input.json", rough_pf = 0.15)
+
+def test_recirc_pebbles():
+    '''
+    Tests recirc_pebbles in main.py
+    '''
+    main.recirc_pebbles("f1_input.json", "rough-pack.txt", 
+                        "f1_main.txt", "lammps/f1main_template.txt")
+    main.recirc_pebbles("f2_input.json", "rough-pack.txt", 
+                        "f2_main.txt", "lammps/f2main_template.txt")
+
+
+def test_cleanup():
+    '''
+    Removes files after testing
+    '''
+    for f in glob.glob("*.txt"):
+        os.remove(f)
+
+
+
+
+
--- tests/test_read_input.py
+++ tests/test_read_input.py
@@ -3,12 +3,13 @@ from ghastly import read_input
 from ghastly import simulation
 from ghastly import core
 
+test_input = read_input.InputBlock("fill_input.json")
+test_sim = test_input.create_obj()
 
 def test_InputBlock():
     '''
     Test InputBlock class.
     '''
-    test_input = read_input.InputBlock("sample_input.json")
 
     assert test_input.sim_var["r_pebble"] == 0.03
     assert test_input.core_inlet_var["inlet_cone"]["zmax"] == 1.1
@@ -21,17 +22,14 @@ def test_create_obj():
     '''
     Test InputBlock's create_obj method.
     '''
-    test_input = read_input.InputBlock("sample_input.json")
-    test_simblock = test_input.create_obj()
 
-    assert type(test_simblock) == simulation.Sim
+    assert type(test_sim) == simulation.Sim
 
 
 def test_create_core_zone():
     '''
     Test InputBlock's create_core_zone method.
     '''
-    test_input = read_input.InputBlock("sample_input.json")
     test_main = test_input.create_core_zone(test_input.core_main_var)
     assert len(test_main) == 2
     assert type(test_main) == dict
@@ -48,12 +46,11 @@ def create_sim_block():
     Tests InputBlock's create_sim_block method.
     '''
 
-    test_input = read_input.InputBlock("sample_input.json")
     test_inlet = test_input.create_core_zone(test_input.core_inlet_var)
     test_main = test_input.create_core_zone(test_input.core_main_var)
     test_outlet = test_input.create_core_zone(test_input.core_outlet_var)
-    test_sim_block = test_input.create_cim_block(test_inlet,
+    test_sim = test_input.create_cim_block(test_inlet,
                                                  test_main,
                                                  test_outlet)
-    assert test_sim_block.pf == 0.60
-    assert test_sim_block.r_pebble == 0.03
+    assert test_sim.pf == 0.60
+    assert test_sim.r_pebble == 0.03
